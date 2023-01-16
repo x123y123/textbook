54bddf08df1b1f8a413a11e0c8b2b5ef85ccd5d5
tony <a0921444212@gmail.com>
2023-01-16 16:21:31 +0800

    add realloc comment and README.md

---
+++ Understanding_and_Using_C_Pointers/experiment/realloc_array/README.md
@@ -0,0 +1,13 @@
+# Realloc Array
+* size_increment: The size of the initial buffer and the amount it will be incremented by when the buffer needs to be enlarged
+* buffer: A pointer to the characters read in
+* current_position: A pointer to the next free position in the buffer
+* max_len: The maximum number of characters that can be safely stored in the buffer
+* len: The number of characters read in
+* character: The last character read in
+
+## If realloc work
+You will get the following response in terminal:
+```shell
+realloc ACT!
+```
--- Understanding_and_Using_C_Pointers/experiment/realloc_array/realloc_array.c
+++ Understanding_and_Using_C_Pointers/experiment/realloc_array/realloc_array.c
@@ -1,8 +1,69 @@
+/* Implement a function to read in characters from standard input and assign them to a buffer. The buffer will contain all of the characters read except for a terminating return character. Since we don't know how many characters the user will input, we don't know how long the buffer should be.*/
+
+// We use the realloc function to allocate additional space by fixed increment amount
+
 #include <stdio.h>
 #include <stdlib.h>
+
+void _safe_free(void **pp)
+{
+    if (pp != NULL && *pp != NULL) {
+        free(*pp);
+        *pp = NULL;
+    }
+}
+
+#define safe_free(p) _safe_free((void**)&p)
+           
 
 char *get_line(void)
 {
     const size_t size_increment = 10;
+    char *buffer = (char*)malloc(size_increment);
+    char *current_position = buffer;     // *current_position = *buff
+    size_t max_len = size_increment;
+    size_t len = 0;
+    int character;
 
+    // error handling, check whether malloc is unable to allocate memory or not
+    if (current_position == NULL) 
+        return NULL;
+    
+    while(1) {
+        // get char
+        character = fgetc(stdin);
+        //printf("%c\n", character);
+        
+        if(character == '\n')
+            break;
+
+        if (++len >= max_len) {
+            // resize 
+            char *new_buffer = realloc(buffer, max_len += size_increment);
+            printf("realloc ACT!\n");
+
+            // error handling, if realloc failed then free up the existing allocated memory and force the function to return NULL
+            if (new_buffer == NULL) {
+               /* free(buffer);
+                  buffer = NULL; */
+                safe_free(buffer);
+                return NULL;
+            }
+
+            // get the new current position
+            current_position = new_buffer + (current_position - buffer);
+            buffer = new_buffer;
+        }
+        *current_position++ = character;
+    }
+    *current_position = '\0';
+    return buffer;
+}
+
+int main()
+{
+    char *input_line = get_line();
+    printf("input_line: %s\n", input_line);
+
+    return 0;
 }
