ce49226bf3dcacabebfadf6a25ca243614922c26
tony <a0921444212@gmail.com>
2023-02-21 00:20:33 +0800

    update add_head()

--- Understanding_and_Using_C_Pointers/experiment/single_linked-list/single_linked-list.c
+++ Understanding_and_Using_C_Pointers/experiment/single_linked-list/single_linked-list.c
@@ -5,8 +5,28 @@ typedef struct _node {
     struct _node* next;
 } Node;
 
-typedef struct _linkedList {
+typedef struct _linked_list {
     Node* head;
     Node* tail;
     Node* current;
 } LinkedList;
+
+void initiallize_list(LinkedList* list)
+{
+    list->head = NULL;
+    list->tail = NULL;
+    list->current = NULL;
+}
+
+void add_head(LinkedList* list, void* data)
+{
+    Node* node = (Node*) malloc(sizeof(Node));
+    node->data = data;
+    if (list->head == NULL) {
+        list->tail = node;
+        node->next = NULL;
+    } else {
+        node->next = list->head;
+    }
+    list->head = node;  // Regardless, the list's head is assigned to the node;
+}
