142a034842afbc3f32487214e08e3275ce2deac5
tony <a0921444212@gmail.com>
2023-02-20 23:59:46 +0800

    update notes

---
+++ Understanding_and_Using_C_Pointers/experiment/single_linked-list/construction_node_part.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+typedef struct _node {
+    void* data;
+    struct _node* next;
+} Node;
+
+typedef struct _linkedList {
+    Node* head;
+    Node* tail;
+    Node* current;
+} LinkedList;
